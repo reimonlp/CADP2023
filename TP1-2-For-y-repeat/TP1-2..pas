bb0e79f3d1788c7e783c1ed27960a705db881cbc
reimon <83924519+reimonlp@users.noreply.github.com>
2023-04-02 14:46:04 -0300

    Create TP1-2..pas

---
+++ TP1-2-For-y-repeat/TP1-2..pas
@@ -0,0 +1,17 @@
+{ 
+}
+program E;
+const
+
+var
+ //DEFINO LAS VARIABLES A UTILIZAR
+begin
+
+  writeln(); //ESPACIO EN BLANCO
+  write('(', i:2, ' de ', numeros,') '); //DECORADOR PARA SABER EL PROGRESO
+  write('Ingrese un numero: '); //SOLICITA INGRESAR UN NUMERO
+  readln(numero); //ALMACENA EL NUMERO INGRESADO EN LA VARIABLE numero
+    
+
+  readln(); //EVITA EL CIERRE DEL PROGRAMA HASTA PRESIONAR ENTER
+end.
