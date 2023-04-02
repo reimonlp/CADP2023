0d7a67bd97f99d698c53767b20c3c2ae230d1fcc
reimon <83924519+reimonlp@users.noreply.github.com>
2023-04-02 14:18:13 -0300

    Create TP1-2.E2.pas

---
+++ TP1-2-For-y-repeat/TP1-2.E2.pas
@@ -0,0 +1,27 @@
+{ 2. Realice un programa que lea 10 números e informe cuál fue el mayor número leído.
+   Por ejemplo, si se lee la secuencia: 3 5 6 2 3 10 98 8 -12 9
+   deberá informar: “El mayor número leído fue el 98”
+}
+program E2;
+const
+  numeros = 10; //DEFINO UNA CONSTANTE numeros
+var
+  numero, i, mayor : integer; //DEFINO LAS VARIABLES A UTILIZAR
+begin
+  // UTILIZA FOR PARA REPETIR LA CANTIDAD DE VECES ASIGNADA EN numeros
+  for i:=1 to numeros do begin
+    write('Ingrese un numero: '); //SOLICITA INGRESAR UN NUMERO
+    
+    readln(numero); //ALMACENA EL NUMERO INGRESADO EN LA VARIABLE numero
+    
+    //SI EL VALOR INGRESADO ES MAYOR QUE LA VARIABLE mayor
+    if (numero > mayor) then
+      mayor := numero; //ACTUALIZA EL VALOR DE mayor
+  end;
+  
+  writeln(); //ESPACIO EN BLANCO
+  
+  writeln('El mayor numero ingresado fue ', mayor);
+  
+  readln(); //EVITA EL CIERRE DEL PROGRAMA HASTA PRESIONAR ENTER
+end.
