d57786d0714b84bc59ada2df2305dad630e34b54
reimon <83924519+reimonlp@users.noreply.github.com>
2023-04-02 13:07:03 -0300

    Update TP1-2.E4B.pas

--- TP1-2-For-y-repeat/TP1-2.E4B.pas
+++ TP1-2-For-y-repeat/TP1-2.E4B.pas
@@ -5,18 +5,15 @@ program E4B;
 var
   num, min1, min2: integer; //DEFINO LAS VARIABLES A UTILIZAR
 begin
-  num:=-1; //ME ASEGURO DE QUE num SEA DISTINTO DE 0
-  
   min1:=9999; min2:=9999; //INICIALIZO LOS MINIMOS CON UN VALOR GRANDE
   
   writeln('Se le solicitará ingresar números. Ingrese el 0 para finalizar.');
+
+  writeln(); //ESPACIO EN BLANCO
+  write('Ingrese un numero: '); //SOLICITA INGRESAR UN NUMERO
+  readln(num); //ALMACENO EL NUMERO INGRESADO EN LA VARIABLE num
+    
   while (num <> 0) do begin 
-    writeln(); //ESPACIO EN BLANCO
-    
-    write('Ingrese un numero: '); //SOLICITA INGRESAR UN NUMERO
-    
-    readln(num); //ALMACENO EL NUMERO INGRESADO EN LA VARIABLE num
-    
     //CASO ES MENOR QUE AMBOS MIN
     if (num < min1) and (num < min2) then begin
       min2:= min1; min1:= num;
@@ -25,6 +22,10 @@ begin
     else
       if (num < min2) then min2:= num;
       
+    writeln(); //ESPACIO EN BLANCO
+    write('Ingrese un numero: '); //SOLICITA INGRESAR UN NUMERO
+    readln(num); //ALMACENO EL NUMERO INGRESADO EN LA VARIABLE num
+    
   end; //TERMINA EL WHILE
   
   writeln(); //ESPACIO EN BLANCO
