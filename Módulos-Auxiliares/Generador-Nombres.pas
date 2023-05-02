40fa4682584efaaffbdddab631b3ec7dbdb19a26
reimon <83924519+reimonlp@users.noreply.github.com>
2023-05-02 12:44:21 -0300

    Create Generador-Nombres.pas

---
+++ Módulos-Auxiliares/Generador-Nombres.pas
@@ -0,0 +1,33 @@
+program GeneradorNombresAleatorios;
+
+const
+  SilabasNombres: array[1..20] of string = ('ana', 'mar', 'jua', 'ped', 'pau', 'car', 'lu', 'ma', 'fe', 'so',
+                                            'li', 'ro', 'me', 'pa', 'no', 'la', 'sa', 'lu', 'ba', 'mi');
+  SilabasApellidos: array[1..20] of string = ('gon', 'mor', 'rom', 'gue', 'fer', 'ram', 'gar', 'ber', 'per', 'del',
+                                              'tor', 'her', 'dia', 'vaz', 'cal', 'sal', 'ver', 'guez', 'ros', 'mart');
+
+procedure GenerarNombreApellidoAleatorio(var Nombre, Apellido: string);
+var
+  LongitudNombre, LongitudApellido, i: integer;
+begin
+  Randomize;
+  LongitudNombre := Random(3) + 2;
+  LongitudApellido := Random(2) + 2;
+  Nombre := ''; Apellido := '';
+  
+  for i := 1 to LongitudNombre do
+    Nombre := Nombre + SilabasNombres[Random(20) + 1];
+
+  for i := 1 to LongitudApellido do
+    Apellido := Apellido + SilabasApellidos[Random(20) + 1];
+
+  Nombre[1] := UpCase(Nombre[1]);
+  Apellido[1] := UpCase(Apellido[1]);
+end;
+
+var
+  Nombre, Apellido: string;
+begin
+  GenerarNombreApellidoAleatorio(Nombre, Apellido);
+  writeln(Nombre, ' ', Apellido);
+end.
