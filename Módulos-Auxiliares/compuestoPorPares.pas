e5e9dfa2ea2302cbd6a951d68a51efc01e82a635
reimon <83924519+reimonlp@users.noreply.github.com>
2023-05-06 20:50:28 -0300

    Create compuestoPorPares.pas

---
+++ Módulos-Auxiliares/compuestoPorPares.pas
@@ -0,0 +1,21 @@
+program compuestoPorPares;
+
+function soloPares(numero:int64): boolean;
+var 
+  esPar: boolean;
+begin
+  esPar := true;
+  while ((numero > 10) And (esPar=true)) Do
+    begin
+      esPar := (numero Mod 2 = 0);
+      numero := numero Div 10;
+    end;
+  soloPares := esPar;
+end;
+
+var
+  numero: int64;
+begin
+  write('Ingrese un número: '); readln(numero);
+  writeln('Compuesto sólo por dígitos pares? ', soloPares(numero));
+end.
