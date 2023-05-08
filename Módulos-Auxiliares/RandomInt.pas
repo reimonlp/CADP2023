7608172deea73bf80de96b150d9876408219bd05
reimon <83924519+reimonlp@users.noreply.github.com>
2023-05-08 01:58:56 -0300

    Create RandomInt.pas

---
+++ Módulos-Auxiliares/RandomInt.pas
@@ -0,0 +1,17 @@
+program RandomInt;
+
+function randomInt(min, max: int64) : int64;
+var
+  d: int64;
+begin
+  if (min < 0) then
+    min := min * -1;
+
+  randomInt := Random(max + min) - min;
+end;
+
+begin
+  write('Ingrese el mínimo deseado: '); readln(min);
+  write('Ingrese el máximo deseado: '); readln(max);
+  writeln(randomInt(min, max));
+end;
